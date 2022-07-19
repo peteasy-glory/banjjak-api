8179f3c2cb91e1e613fe0dfb7db398148b967b68
eaden <eaden@peteasy.kr>
2022-07-19 09:53:04 +0900

    .

--- banjjak_partner/wsgi.py
+++ banjjak_partner/wsgi.py
@@ -4,10 +4,15 @@ WSGI config for banjjak_partner project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
