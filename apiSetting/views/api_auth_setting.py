a46f344fdebaa75cfa0b8f54f61718fca3060d19
glory <glory@peteasy.kr>
2022-07-28 09:05:22 +0900

    apiSetting/api_auth_setting

--- apiSetting/views/api_auth_setting.py
+++ apiSetting/views/api_auth_setting.py
@@ -21,7 +21,7 @@ class TAuthSetting(TAPIBase):
         try:
             if partner_id is None:
                 return HttpResponse(self.json.dicToJson(self.message.errorBadRequst()))
-            data, rows, columns = self.db.resultDBQuery(PROC_SHOP_NAME_GET % (partner_id), QUERY_DB)
+            data, rows, columns = self.db.resultDBQuery(PROC_SETTING_ARTIST_GET % (partner_id), QUERY_DB)
             ret = self.message.successOk()
             if data is None:
                 ret["body"] = {}
@@ -33,21 +33,21 @@ class TAuthSetting(TAPIBase):
             print(e)
             return HttpResponse(self.json.dicToJson(self.message.error(e.args[1])))
 
-    def get(self, request, year, month, partner_id):
-        try:
-            if partner_id is None:
-                return HttpResponse(self.json.dicToJson(self.message.errorBadRequst()))
-            data, rows, columns = self.db.resultDBQuery(PROC_SHOP_NAME_GET % (partner_id), QUERY_DB)
-            ret = self.message.successOk()
-            if data is None:
-                ret["body"] = {}
-                return HttpResponse(self.json.dicToJson(ret))
-            body = self.queryDataToDic(data, rows, columns)
-            ret["body"] = body
-            return HttpResponse(self.json.dicToJson(ret))
-        except Exception as e:
-            print(e)
-            return HttpResponse(self.json.dicToJson(self.message.error(e.args[1])))
+    # def get(self, request, year, month, partner_id):
+    #     try:
+    #         if partner_id is None:
+    #             return HttpResponse(self.json.dicToJson(self.message.errorBadRequst()))
+    #         data, rows, columns = self.db.resultDBQuery(PROC_SHOP_NAME_GET % (partner_id), QUERY_DB)
+    #         ret = self.message.successOk()
+    #         if data is None:
+    #             ret["body"] = {}
+    #             return HttpResponse(self.json.dicToJson(ret))
+    #         body = self.queryDataToDic(data, rows, columns)
+    #         ret["body"] = body
+    #         return HttpResponse(self.json.dicToJson(ret))
+    #     except Exception as e:
+    #         print(e)
+    #         return HttpResponse(self.json.dicToJson(self.message.error(e.args[1])))
 
 
     def post(self, request):
