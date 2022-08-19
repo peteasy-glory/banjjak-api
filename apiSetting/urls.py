e97e3f2a1e2cfaea08e06e1415ac7258e53199ab
glory <glory@peteasy.kr>
2022-08-19 16:22:32 +0900

    판매상품관리 미용쿠폰 get 완료

--- apiSetting/urls.py
+++ apiSetting/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from apiSetting.views import api_artist_work, api_open_close, api_regular_holiday, api_artist_vacation, api_part_time, \
     api_break_time, api_artist_setting, api_schedule_artist, api_auth_setting, api_reserve, api_pay_type, api_beauty_product, \
-    api_beauty_option
+    api_beauty_option, api_beauty_coupon
 # from apiSetting.views import api_artist_work, api_schedule_artist
 
 
@@ -29,5 +29,6 @@ urlpatterns = [
     path('setting/pay-type', api_pay_type.TPayType.as_view()),
     path('setting/beauty-product/<str:partner_id>', api_beauty_product.TProduct.as_view()),
     path('setting/option-product/<str:partner_id>', api_beauty_option.TProduct.as_view()),
+    path('setting/beauty-coupon/<str:partner_id>', api_beauty_coupon.TCoupon.as_view()),
 
 ]
